c4dd8dd5e82a9f45cc451b0e402f988ecddf4061
Kovalev Maksim <teasec4@yandex.ru>
2026-05-24 19:41:38 +0800

    adding harness without agent

---
+++ internal/harness/harness.go
@@ -0,0 +1,19 @@
+package harness
+
+import "ai-agent/internal/loop"
+
+type HarnessExecutionResult struct{
+	LoopResult loop.LoopResult
+	Task string
+}
+
+func RunHarness(task string) HarnessExecutionResult{
+	// create tools
+
+
+	// create Context
+
+
+	// result
+	return HarnessExecutionResult{}
+}
